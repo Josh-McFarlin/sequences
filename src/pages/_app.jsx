1d55e13f3a7993b4a84fb88c808ac75ac246546b
Josh-McFarlin <jmcfarlin6@gatech.edu>
2022-02-08 15:09:14 -0500

    3 files modified

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -12,7 +12,11 @@ const MyApp = ({ Component, pageProps }) => (
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
       />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
