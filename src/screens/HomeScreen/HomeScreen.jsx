3f2babad26f71182c55317413a7c2279d4b7d0ff
Josh-McFarlin <jmcfarlin6@gatech.edu>
2022-03-07 15:58:59 -0500

    2 files modified

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import urls from "../../utils/urls";
 import classes from "./HomeScreen.module.scss";
 
-const HomeScreen = () => (
-  <div className={clsx(classes.root)}>
-    <h1>Welcome to our study</h1>
-    <h3>Team Tempo</h3>
-    <Link href={urls.intro}>Start Test</Link>
-  </div>
-);
+const HomeScreen = () => {
+  const router = useRouter();
+
+  return (
+    <div className={clsx(classes.root)}>
+      <h1>Welcome to our study</h1>
+      <h3>Team Tempo</h3>
+      <Link
+        href={{
+          pathname: urls.intro,
+          query: router.query || {},
+        }}
+      >
+        Start Test
+      </Link>
+    </div>
+  );
+};
 
 export default HomeScreen;
--- src/screens/IntroScreen/IntroScreen.jsx
+++ src/screens/IntroScreen/IntroScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import SwipeableViews from "react-swipeable-views";
 import { bindKeyboard } from "react-swipeable-views-utils";
 import urls from "../../utils/urls";
@@ -8,28 +9,42 @@ import classes from "./IntroScreen.module.scss";
 
 const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 
-const IntroScreen = () => (
-  <BindKeyboardSwipeableViews className={clsx(classes.root)} enableMouseEvents>
-    <div className={classes.page}>
-      <h3>The following excercise will be testing your memory.</h3>
-      <p className={classes.swipeNotif}>Please swipe →</p>
-    </div>
-    <div className={classes.page}>
-      <h3>You will be shown a sequence of numbers.</h3>
-      <h4>Please memorize these numbers in order.</h4>
-    </div>
-    <div className={classes.page}>
-      <h3>
-        Once all numbers have been shown, you will be asked to recall them in
-        their order of presentation.
-      </h3>
-      <h4>If you cannot remember, press "I Forgot"</h4>
-    </div>
-    <div className={classes.page}>
-      <h3>When you are ready to begin, press the button below:</h3>
-      <Link href={urls.test}>Start Test</Link>
-    </div>
-  </BindKeyboardSwipeableViews>
-);
+const IntroScreen = () => {
+  const router = useRouter();
+
+  return (
+    <BindKeyboardSwipeableViews
+      className={clsx(classes.root)}
+      enableMouseEvents
+    >
+      <div className={classes.page}>
+        <h3>The following excercise will be testing your memory.</h3>
+        <p className={classes.swipeNotif}>Please swipe →</p>
+      </div>
+      <div className={classes.page}>
+        <h3>You will be shown a sequence of numbers.</h3>
+        <h4>Please memorize these numbers in order.</h4>
+      </div>
+      <div className={classes.page}>
+        <h3>
+          Once all numbers have been shown, you will be asked to recall them in
+          their order of presentation.
+        </h3>
+        <h4>If you cannot remember, press "I Forgot"</h4>
+      </div>
+      <div className={classes.page}>
+        <h3>When you are ready to begin, press the button below:</h3>
+        <Link
+          href={{
+            pathname: urls.test,
+            query: router.query || {},
+          }}
+        >
+          Start Test
+        </Link>
+      </div>
+    </BindKeyboardSwipeableViews>
+  );
+};
 
 export default IntroScreen;
